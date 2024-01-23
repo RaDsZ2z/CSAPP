a1017db7b935c9deb15c177c3d9a49216ac35012
RaDsZ2z <129292565+RaDsZ2z@users.noreply.github.com>
2024-01-23 16:35:52 +0800

    Update csapp.h

--- csapp.h
+++ csapp.h
@@ -5,4 +5,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+void unix_error(const char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
 #endif
