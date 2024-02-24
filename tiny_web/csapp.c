bb3e641e6f40a27262e095a83cb6f3de25bb3f8e
RaDsZ2z <129292565+RaDsZ2z@users.noreply.github.com>
2024-02-24 22:41:25 +0800

    Create csapp.c

---
+++ tiny_web/csapp.c
@@ -0,0 +1,291 @@
+#include "csapp.h"
+/* $begin open_listenfd */
+int open_listenfd(char *port)
+{
+    struct addrinfo hints, *listp, *p;
+    int listenfd, rc, optval = 1;
+
+    /* Get a list of potential server addresses */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;             /* Accept connections */
+    hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG; /* ... on any IP address */
+    hints.ai_flags |= AI_NUMERICSERV;            /* ... using port number */
+    if ((rc = getaddrinfo(NULL, port, &hints, &listp)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo failed (port %s): %s\n", port, gai_strerror(rc));
+        return -2;
+    }
+
+    /* Walk the list for one that we can bind to */
+    for (p = listp; p; p = p->ai_next)
+    {
+        /* Create a socket descriptor */
+        if ((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+            continue; /* Socket failed, try the next */
+
+        /* Eliminates "Address already in use" error from bind */
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, // line:netp:csapp:setsockopt
+                   (const void *)&optval, sizeof(int));
+
+        /* Bind the descriptor to the address */
+        if (bind(listenfd, p->ai_addr, p->ai_addrlen) == 0)
+            break; /* Success */
+        if (close(listenfd) < 0)
+        { /* Bind failed, try the next */
+            fprintf(stderr, "open_listenfd close failed: %s\n", strerror(errno));
+            return -1;
+        }
+    }
+
+    /* Clean up */
+    freeaddrinfo(listp);
+    if (!p) /* No address worked */
+        return -1;
+
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, LISTENQ) < 0)
+    {
+        close(listenfd);
+        return -1;
+    }
+    return listenfd;
+}
+/* $end open_listenfd */
+
+int Open_listenfd(char *port)
+{
+    int rc;
+
+    if ((rc = open_listenfd(port)) < 0)
+        unix_error("Open_listenfd error");
+    return rc;
+}
+int Accept(int s, struct sockaddr *addr, socklen_t *addrlen)
+{
+    int rc;
+
+    if ((rc = accept(s, addr, addrlen)) < 0)
+        unix_error("Accept error");
+    return rc;
+}
+void unix_error(const char *msg) /* Unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+/* $end unixerror */
+
+void posix_error(int code, const char *msg) /* Posix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(code));
+    exit(0);
+}
+
+void gai_error(int code, const char *msg) /* Getaddrinfo-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, gai_strerror(code));
+    exit(0);
+}
+
+void app_error(const char *msg) /* Application error */
+{
+    fprintf(stderr, "%s\n", msg);
+    exit(0);
+}
+/* $end errorfuns */
+
+void dns_error(const char *msg) /* Obsolete gethostbyname error */
+{
+    fprintf(stderr, "%s\n", msg);
+    exit(0);
+}
+
+void Getnameinfo(const struct sockaddr *sa, socklen_t salen, char *host,
+                 size_t hostlen, char *serv, size_t servlen, int flags)
+{
+    int rc;
+
+    if ((rc = getnameinfo(sa, salen, host, hostlen, serv,
+                          servlen, flags)) != 0)
+        gai_error(rc, "Getnameinfo error");
+}
+
+void Close(int fd)
+{
+    int rc;
+
+    if ((rc = close(fd)) < 0)
+        unix_error("Close error");
+}
+
+void rio_readinitb(rio_t *rp, int fd)
+{
+    rp->rio_fd = fd;
+    rp->rio_cnt = 0;
+    rp->rio_bufptr = rp->rio_buf;
+}
+void Rio_readinitb(rio_t *rp, int fd)
+{
+    rio_readinitb(rp, fd);
+}
+
+/* $begin rio_read */
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
+{
+    int cnt;
+
+    while (rp->rio_cnt <= 0)
+    { /* Refill if buf is empty */
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
+                           sizeof(rp->rio_buf));
+        if (rp->rio_cnt < 0)
+        {
+            if (errno != EINTR) /* Interrupted by sig handler return */
+                return -1;
+        }
+        else if (rp->rio_cnt == 0) /* EOF */
+            return 0;
+        else
+            rp->rio_bufptr = rp->rio_buf; /* Reset buffer ptr */
+    }
+
+    /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
+    cnt = n;
+    if (rp->rio_cnt < n)
+        cnt = rp->rio_cnt;
+    memcpy(usrbuf, rp->rio_bufptr, cnt);
+    rp->rio_bufptr += cnt;
+    rp->rio_cnt -= cnt;
+    return cnt;
+}
+/* $end rio_read */
+
+/*
+ * rio_readlineb - Robustly read a text line (buffered)
+ */
+/* $begin rio_readlineb */
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    int n, rc;
+    char c, *bufp = (char *)usrbuf;
+
+    for (n = 1; n < maxlen; n++)
+    {
+        if ((rc = rio_read(rp, &c, 1)) == 1)
+        {
+            *bufp++ = c;
+            if (c == '\n')
+            {
+                n++;
+                break;
+            }
+        }
+        else if (rc == 0)
+        {
+            if (n == 1)
+                return 0; /* EOF, no data read */
+            else
+                break; /* EOF, some data was read */
+        }
+        else
+            return -1; /* Error */
+    }
+    *bufp = 0;
+    return n - 1;
+}
+// /* $end rio_readlineb */
+ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    ssize_t rc;
+
+    if ((rc = rio_readlineb(rp, usrbuf, maxlen)) < 0)
+        unix_error("Rio_readlineb error");
+    return rc;
+}
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = (char *)usrbuf;
+
+    while (nleft > 0)
+    {
+        if ((nwritten = write(fd, bufp, nleft)) <= 0)
+        {
+            if (errno == EINTR) /* Interrupted by sig handler return */
+                nwritten = 0;   /* and call write() again */
+            else
+                return -1; /* errno set by write() */
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
+    }
+    return n;
+}
+
+void Rio_writen(int fd, void *usrbuf, size_t n)
+{
+    if (rio_writen(fd, usrbuf, n) != n)
+        unix_error("Rio_writen error");
+}
+
+pid_t Fork(void)
+{
+    pid_t pid;
+
+    if ((pid = fork()) < 0)
+        unix_error("Fork error");
+    return pid;
+}
+
+int Dup2(int fd1, int fd2)
+{
+    int rc;
+
+    if ((rc = dup2(fd1, fd2)) < 0)
+        unix_error("Dup2 error");
+    return rc;
+}
+
+void Execve(const char *filename, char *const argv[], char *const envp[])
+{
+    if (execve(filename, argv, envp) < 0)
+        unix_error("Execve error");
+}
+/* $begin wait */
+pid_t Wait(int *status)
+{
+    pid_t pid;
+
+    if ((pid = wait(status)) < 0)
+        unix_error("Wait error");
+    return pid;
+}
+/* $end wait */
+
+int Open(const char *pathname, int flags, mode_t mode)
+{
+    int rc;
+
+    if ((rc = open(pathname, flags, mode)) < 0)
+        unix_error("Open error");
+    return rc;
+}
+
+/***************************************
+ * Wrappers for memory mapping functions
+ ***************************************/
+void *Mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+{
+    void *ptr;
+
+    if ((ptr = mmap(addr, len, prot, flags, fd, offset)) == ((void *)-1))
+        unix_error("mmap error");
+    return (ptr);
+}
+void Munmap(void *start, size_t length)
+{
+    if (munmap(start, length) < 0)
+        unix_error("munmap error");
+}
