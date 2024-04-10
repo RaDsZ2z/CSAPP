ea23f220c13bcf1dfd34f317514d616f1ba0986b
RaDsZ2z <129292565+RaDsZ2z@users.noreply.github.com>
2024-04-10 16:31:32 +0800

    Update p7_mm.c

--- p7_mm.c
+++ p7_mm.c
@@ -70,138 +70,6 @@ team_t team = {
 
 static char *heap_listp;
 /*
- * mm_init - initialize the malloc package.
- */
-int mm_init(void)
-{
-    // void *mem_sbrk(int incr)
-    // 申请incr大小的内存并返回内存首地址
-    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
-        return -1;
-    PUT(heap_listp, 0);
-    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
-    heap_listp += (2 * WSIZE);
-
-    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
-        return -1;
-    return 0;
-}
-
-/*
-当申请了words个字的空间，使其内存对齐到双字(8字节)
- */
-static void *extned_heap(size_t words)
-{
-    char *bp;
-    size_t size;
-
-    // size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-    size = ALIGN(words * WSIZE);
-    if ((long)(bp = mem_sbrk(size)) == -1)
-        return NULL;
-    // mem_sbrk返回的是p，但是下面把它变成bp了
-    // 此时bp指向图9-42结尾块的末尾
-    PUT(HDRP(bp), PACK(size, 0));         // 把结尾块变成自己的hdr
-    PUT(FTRP(bp), PACK(size, 0));         // 紧跟着hdr添加ftr
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // 在ftr后面添加结尾块
-    return coalesce(bp);                  // 周围有内存碎片则合并
-}
-/*
-// 遍历内存块，找到第一个能放下的则返回其地址
-*/
-static void *find_fit(size_t asize)
-{
-
-    void *bp;
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
-    {
-        if (!GET_ALLOC(HERP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
-        {
-            return bp;
-        }
-    }
-    // 找不到适合的
-    return NULL;
-}
-/*
-在当前位置申请了asize字节空间
-剩下的块大于等于16字节则分割
- */
-static void place(void *bp, size_t asize)
-{
-    size_t csize = GET_SIZE(HRDP(bp));
-
-    if ((csize - asize) >= (2 * DSIZE))
-    {
-        PUT(HDRP(bp), PACK(asize, 1));
-        PUT(FTRP(bp), PACK(asize, 1));
-        bp = NEXT_BLKP(bp);
-        PUT(HDRP(bp), PACK(csize - asize, 0));
-        PUT(FTRP(bp), PACK(csize - asize, 0));
-    }
-    else
-    {
-        PUT(HDRP(bp), PACK(csize, 1));
-        PUT(FTRP(bp), PACK(csize, 1));
-    }
-}
-/*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
-void *mm_malloc(size_t size)
-{
-    // rewrite
-    size_t asize;
-    size_t extendsize;
-    char *bp;
-
-    if (size == 0)
-        return NULL;
-
-    // 请求的空间必须有头部脚部，占了8字节，且必须有有效载荷满足双字对齐
-    // 所以最小空间是2*DSIZE 16字节
-    // 若请求的有效载荷空间大于8字节则加上头部脚部的8字节然后满足双字对齐
-
-    /*
-    if (size <= DSIZE)
-        asize = 2 * DSIZE;
-    else
-        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
-    */
-    asize = ALIGN(asize);
-    asize += DSIZE;
-
-    if (bp = find_fit(asize) != NULL)
-    {
-        place(bp, asize);
-        return bp;
-    }
-
-    // 找不到合适的块则需要请求额外的内存空间
-    extendsize = MAX(asize, CHUNKSIZE);
-    if ((bp = extemd_heap(extendsize / WSIZE)) == NULL)
-        return NULL;
-    place(bp, asize);
-    return bp;
-}
-
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void *ptr)
-{
-    // write by myself
-    size_t size = GET_SIZE(ptr);
-    PUT(ptr, PACK(size, 0));
-    PUT((char *)ptr + size - WSIZE, PACK(size, 0));
-
-    return coalesce((char *)ptr + WSIZE);
-}
-
-/*
 //合并内存碎片
 */
 static void *coalesce(void *bp)
@@ -244,6 +112,138 @@ static void *coalesce(void *bp)
     }
     return bp;
 }
+/*
+当申请了words个字的空间，使其内存对齐到双字(8字节)
+ */
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    // size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    size = ALIGN(words * WSIZE);
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    // mem_sbrk返回的是p，但是下面把它变成bp了
+    // 此时bp指向图9-42结尾块的末尾
+    PUT(HDRP(bp), PACK(size, 0));         // 把结尾块变成自己的hdr
+    PUT(FTRP(bp), PACK(size, 0));         // 紧跟着hdr添加ftr
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // 在ftr后面添加结尾块
+    return coalesce(bp);                  // 周围有内存碎片则合并
+}
+/*
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void)
+{
+    // void *mem_sbrk(int incr)
+    // 申请incr大小的内存并返回内存首地址
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+    heap_listp += (2 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
+    return 0;
+}
+
+/*
+// 遍历内存块，找到第一个能放下的则返回其地址
+*/
+static void *find_fit(size_t asize)
+{
+
+    void *bp;
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
+    {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
+        {
+            return bp;
+        }
+    }
+    // 找不到适合的
+    return NULL;
+}
+/*
+在当前位置申请了asize字节空间
+剩下的块大于等于16字节则分割
+ */
+static void place(void *bp, size_t asize)
+{
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if ((csize - asize) >= (2 * DSIZE))
+    {
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+    }
+    else
+    {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
+/*
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void *mm_malloc(size_t size)
+{
+    // rewrite
+    size_t asize;
+    size_t extendsize;
+    char *bp;
+
+    if (size == 0)
+        return NULL;
+
+    // 请求的空间必须有头部脚部，占了8字节，且必须有有效载荷满足双字对齐
+    // 所以最小空间是2*DSIZE 16字节
+    // 若请求的有效载荷空间大于8字节则加上头部脚部的8字节然后满足双字对齐
+
+    /*
+    if (size <= DSIZE)
+        asize = 2 * DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+    */
+    asize = ALIGN(size);
+    asize += DSIZE;
+
+    if ((bp = find_fit(asize)) != NULL)
+    {
+        place(bp, asize);
+        return bp;
+    }
+
+    // 找不到合适的块则需要请求额外的内存空间
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr)
+{
+    // write by myself
+    size_t size = GET_SIZE(ptr);
+    PUT(ptr, PACK(size, 0));
+    PUT((char *)ptr + size - WSIZE, PACK(size, 0));
+
+    coalesce((char *)ptr + WSIZE);
+}
+
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
@@ -304,7 +304,7 @@ void *mm_realloc(void *ptr, size_t size)
             // 不够则申请新的空间
             // 申请新的空间失败 返回-1
             void *p = mm_malloc(size);
-            if (p == -1)
+            if (p == (void *)-1)
             {
                 return (void *)-1;
             }
